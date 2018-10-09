f34071da16322aa39616875f7bb817dba8c897a3
Daniel <daniel.sendmemail@gmail.com>
2018-10-08 21:36:07 -0500

    javascript_creado

---
+++ js/index.js
@@ -0,0 +1,17 @@
+const app = document.getElementById('app')
+const a = document.getElementById('a')
+const b = document.getElementById('b')
+const c = document.getElementById('c')
+const d = document.getElementById('d')
+const e = document.getElementById('d')
+const f = document.getElementById('d')
+const g = document.getElementById('d')
+
+
+
+const e
+
+
+
+let el = `hola`
+app.insertAdjacentHTML("beforeend", el)
