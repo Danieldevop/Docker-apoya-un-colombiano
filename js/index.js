bcfcfe6d67a3af3531aa0a5868a28ec8c5bcc179
Daniel <daniel.sendmemail@gmail.com>
2018-10-08 22:22:22 -0500

    Añadí 6 imagenes que se conmutan al hace click en categorias

--- js/index.js
+++ js/index.js
@@ -1,17 +1,35 @@
-const app = document.getElementById('app')
-const a = document.getElementById('a')
+function a() {
+  const app = document.getElementById('app')
+  app.innerHTML = `
+    <div>
+      <img src = "https://png.icons8.com/material/50/000000/money.png" >
+    </div>
+    <div>
+      <img src = "https://png.icons8.com/material/50/000000/money.png" >
+    </div>
+    <div>
+      <img src = "https://png.icons8.com/material/50/000000/money.png" >
+    </div>
+    <div>
+      <img src = "https://png.icons8.com/material/50/000000/money.png" >
+    </div>
+    <div>
+      <img src = "https://png.icons8.com/material/50/000000/money.png" >
+    </div>
+    <div>
+      <img src = "https://png.icons8.com/material/50/000000/money.png" >
+    </div>
+  `
+
+}
+
+
 const b = document.getElementById('b')
 const c = document.getElementById('c')
 const d = document.getElementById('d')
-const e = document.getElementById('d')
-const f = document.getElementById('d')
-const g = document.getElementById('d')
-
-
-
-const e
-
-
-
-let el = `hola`
-app.insertAdjacentHTML("beforeend", el)
+const e = document.getElementById('e')
+const f = document.getElementById('f')
+const g = document.getElementById('g')
+const h = document.getElementById('h')
+const i = document.getElementById('i')
+const j = document.getElementById('j')
